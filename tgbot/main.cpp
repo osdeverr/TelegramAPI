3634d20e215391943385716454b356c9321e5207
Nikita Ivanov <krasota156@gmail.com>
2020-03-17 18:06:23 +0300

    Initial Commit

---
+++ tgbot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tgbot
+//
+//  Created by Nikita Ivanov on 3/17/20.
+//  Copyright © 2020 Plak Corp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
